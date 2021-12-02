4ab0c49e8505a8f47c11aff218d8422c0a8cd1e8
Daniel Rodrigues <55995669+TheAughat@users.noreply.github.com>
2021-12-02 11:40:12 +0000

    created first file

---
+++ app.py
@@ -0,0 +1,35 @@
+import random as rand
+
+def print_matrix():
+  for row in matrix():
+    newstr_arr = str(row).split(',')
+    newstr = ''
+    for item in newstr_arr:
+      newstr += item
+    print(newstr)
+
+
+def main():
+  matrix = [[]]
+  for n in range(5):
+    for m in range(5):
+      matrix[n].append(0)
+    matrix.append([])
+  matrix.pop()
+  print_matrix()
+  print()
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      rand_num = rand.randint(0, 9)
+      matrix[i][j] = rand_num
+      matrix[j][i] = rand_num
+  print_matrix()
+  print()
+  for i in range(len(matrix)):
+    matrix[i][i] = 0
+  print_matrix()
+  print()
+
+
+if __name__ == '__main__':
+  main()
